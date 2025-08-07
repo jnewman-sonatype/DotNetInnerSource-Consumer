5b57e37779e28ae4d4cac5df6e304f4493fd8651
James Newman <99325304+jnewman-sonatype@users.noreply.github.com>
2025-08-07 17:43:19 +0100

    Added source

---
+++ DotNetInnerSource-Consumer/Consumer.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetInnerSource.Consumer
+{
+    public class Consumer
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("DotNetInnerSource.Consumer says Hello, world!");
+
+            Producer.Producer p = new();
+            p.EntryPoint();
+
+        }
+    }
+}
